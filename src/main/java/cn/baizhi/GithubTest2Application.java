81d0cdc18b5dd0c7c6cceb08ef98630d93932add
sxx <1056018026@qq.com>
2021-08-24 11:17:58 +0800

    这是第二个github項目v1.0

---
+++ src/main/java/cn/baizhi/GithubTest2Application.java
@@ -0,0 +1,13 @@
+package cn.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubTest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/baizhi/GithubTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.baizhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
